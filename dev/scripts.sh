7e18dde655b7ad9058d525558cf1cb00ee7761ff
Justin stander <justin.stander@gmail.com>
2024-07-22 21:56:04 -0400

    html page head

--- dev/scripts.sh
+++ dev/scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-function create_build_folder() {
-  echo 'create build folder'
+function create_build_directory() {
+  echo 'create build directory'
   mkdir -p build
 }
 
@@ -10,22 +10,43 @@ function copy_assets() {
   cp -r src/public/* build
 }
 
+function insert_contents() {
+  find build -type f -name "*.html" -exec sed -i -e "/$1/r $2" {} +
+}
+
 function build_css() {
   echo 'build css'
-  find build -type f -name "*.html" -exec sed -i -e '/\/\*style\*\//r src/style.css' {} +
+  insert_contents "\/\*style\*\/" "src/style.css"
+}
+
+function build_head() {
+  echo 'build head'
+  insert_contents "<!-- head -->" "src/head.html" 
 }
 
 function build() {
   echo 'build...'
-  create_build_folder &&
+  create_build_directory &&
     copy_assets &&
+    build_head &&
     build_css &&
     echo '...done'
 }
 
+function clean() {
+  echo 'removing build directory'
+  rm -rf build
+}
+
+function dev() {
+  echo 'starting dev server'
+  build &&
+    (cd build/ && ws --https)
+}
+
 case "$1" in
 "") ;;
-build)
+build | clean | dev)
   "$@"
   exit
   ;;
